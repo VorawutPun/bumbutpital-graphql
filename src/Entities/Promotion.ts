40493a0bcea4fc91c7014a5f2a87bf7d4b125661
Vorawut <vorawut.pun@mail.kmutt.ac.th>
2021-11-18 01:00:11 +0700

    อะไรไม่รู้ promotion แก้หน้า

--- src/Entities/Promotion.ts
+++ src/Entities/Promotion.ts
@@ -15,27 +15,27 @@ export class Promotion extends BaseEntity {
   @Column()
   createAt!: string;
 
-  @Column({length:5000})
+  @Column({ length: 5000 })
   title!: string;
 
-  @Column({length:5000})
+  @Column({ length: 5000 })
   hospitalDetail!: string;
 
   @Column({
     type: "varchar",
-  nullable: false,
-  transformer: new EncryptionTransformer({
-    key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
-    algorithm: 'aes-256-cbc',
-    ivLength: 16,
-    iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    nullable: false,
+    transformer: new EncryptionTransformer({
+      key: "e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61",
+      algorithm: "aes-256-cbc",
+      ivLength: 16,
+      iv: "ff5ac19190424b1d88f9419ef949ae56",
+    }),
   })
-})
   couponCode!: string | undefined;
 
   @Column()
   Url!: string;
 
-  @Column({length:1000})
+  @Column({ length: 5000 })
   expiredDate!: string;
 }
