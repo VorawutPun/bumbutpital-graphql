c5c4d6ba66430b586e77581dd985574cb0e23507
TAE <aekknon.phomsiriudom@mail.kmutt.ac.th>
2021-11-22 14:38:11 +0700

    [Fix] wong leb

--- src/Entities/Forum.ts
+++ src/Entities/Forum.ts
@@ -6,30 +6,32 @@ export class Forum extends BaseEntity {
   forumID!: string;
 
   @Column()
-  userID!:string;
+  userID!: string;
 
   @Column({ nullable: true })
   staffID!: string;
 
   @Column({
     type: "longtext",
-  nullable: true,
-  transformer: new EncryptionTransformer({
-    key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
-    algorithm: 'aes-256-cbc',
-    ivLength: 16,
-    iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    nullable: true,
+    transformer: new EncryptionTransformer({
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+      ivLength: 16,
+      iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    })
   })
   title!: string | undefined;
 
   @Column({
     type: "longtext",
-  nullable: true,
-  transformer: new EncryptionTransformer({
-    key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
-    algorithm: 'aes-256-cbc',
-    ivLength: 16,
-    iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    nullable: true,
+    transformer: new EncryptionTransformer({
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+      ivLength: 16,
+      iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    })
   })
   description!: string | undefined;
 
@@ -38,12 +40,13 @@ export class Forum extends BaseEntity {
 
   @Column({
     type: "longtext",
-  nullable: true,
-  transformer: new EncryptionTransformer({
-    key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
-    algorithm: 'aes-256-cbc',
-    ivLength: 16,
-    iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    nullable: true,
+    transformer: new EncryptionTransformer({
+      key: 'e41c966f21f9e1577802463f8924e6a3fe3e9751f201304213b2f845d8841d61',
+      algorithm: 'aes-256-cbc',
+      ivLength: 16,
+      iv: 'ff5ac19190424b1d88f9419ef949ae56'
+    })
   })
   answer!: string | undefined;
-  }
+}
